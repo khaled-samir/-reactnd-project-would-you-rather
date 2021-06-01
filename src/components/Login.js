3a4756c55802cee07b4af83957988663fc12b249
khaled <khaled@zealous.co>
2021-06-02 01:48:42 +0200

    Fix Redirect Component issue

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setAuthedUser } from '../actions/authedUser';
+import { useHistory } from "react-router-dom";
 
 
-class Login extends Component {
-    componentDidMount() {
+function Login(props) {
+    let history = useHistory();
 
-    }
 
-    render() {
-        const { store, state, dispatch } = this.props;
+    const { store, state, dispatch, location } = props;
 
-        return (
+    return (
 
-            <div className="col-xs-offset-3 col-xs-6 block-center log-ing">
+        <div className="col-xs-offset-3 col-xs-6 block-center log-ing">
 
-                <h3>Welcome to the Would You Rather App!</h3>
+            <h3>Welcome to the Would You Rather App!</h3>
 
-                <h3>Please sign in to continue</h3>
+            <h3>Please sign in to continue</h3>
 
-                <label htmlFor="login">sign in</label>
-                <div className="input-group width-100">
-                    <form onSubmit={(e) => {
-                        e.preventDefault()
-                        const user = e.target[0].value
-                        // alert(user)
-                        dispatch(setAuthedUser(user))
-                    }}>
+            <label htmlFor="login">sign in</label>
+            <div className="input-group width-100">
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    const user = e.target[0].value
 
-                        <select className="form-control" id="login">
-                            {
-                                Object.keys(state.users).map((userName) => {
-                                    const user = state.users[userName]
-                                    return (
-                                        <option value={user.id}
-                                            key={user.id}>
-                                            {user.name}
-                                        </option>)
-                                })
-                            }
+                    dispatch(setAuthedUser(user))
 
-                        </select>
-                        <button type="submit" className="btn btn-group-justified btn-primary" >Sign in!</button>
-                    </form>
-                </div>
+                    if (history) {
+                        if (history.location === undefined) {
+                            let referrer = history.state.state.referrer;
+                        }
+                        let referrer = history.location.state.referrer;
+                        history.push(referrer)
+                    }
+
+                }}>
+
+                    <select className="form-control" id="login">
+                        {
+                            Object.keys(state.users).map((userName) => {
+                                const user = state.users[userName]
+                                return (
+                                    <option value={user.id}
+                                        key={user.id}>
+                                        {user.name}
+                                    </option>)
+                            })
+                        }
+
+                    </select>
+                    <button type="submit" className="btn btn-group-justified btn-primary" >Sign in!</button>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default connect((state) => ({
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,11 +1,20 @@
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router";
 
+import { useHistory } from "react-router-dom";
+
 
 
 function PrivateRoute(props) {
+    let history = useHistory();
+    let currentLocation = history.location.pathname
+    // debugger
     if (props.authedUser === '') {
-        return <Redirect to="/" />
+        // return <Redirect to="/" />
+        return <Redirect to={{
+            pathname: '/',
+            state: { referrer: currentLocation }
+        }} />
     }
     return <Route {...props} />
 
