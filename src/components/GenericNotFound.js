751e996a0c8ffaf3f45f9e05e24cae5bc517d044
khaled <khaled@zealous.co>
2021-06-02 02:22:29 +0200

    Add go to home link for GenericNotFound.js

--- src/components/GenericNotFound.js
+++ src/components/GenericNotFound.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class GenericNotFound extends Component {
     render() {
         return (
             <h1>
                 Not Found 404
+                <Link to="/"> Go Home</Link>
             </h1>
         );
     }
